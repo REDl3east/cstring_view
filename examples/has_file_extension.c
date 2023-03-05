13f460de42692af2db6a16d59a517f99fbb6ed91
Dalton Overmyer <overmyerms13dalton@gmail.com>
2023-03-04 21:14:06 -0600

    renamed function

--- examples/has_file_extension.c
+++ examples/has_file_extension.c
@@ -2,27 +2,27 @@
 
 #include <stdio.h>
 
-int has_file_html_extension(const char* file) {
+int has_html_extension(const char* file) {
   return sv_ends_with(sv_create_from_cstr(file), sv(".html"));
 }
 
 int main() {
   const char* file = "index.html";
-  if (has_file_html_extension(file)) {
+  if (has_html_extension(file)) {
     printf("'%s' has an .html extension.\n", file);
   } else {
     printf("'%s' does not have an .html extension.\n", file);
   }
 
   file = "index.css";
-  if (has_file_html_extension(file)) {
+  if (has_html_extension(file)) {
     printf("'%s' has an .html extension.\n", file);
   } else {
     printf("'%s' does not have an .html extension.\n", file);
   }
 
   file = "/home/dalton/documents/home.html";
-  if (has_file_html_extension(file)) {
+  if (has_html_extension(file)) {
     printf("'%s' has an .html extension.\n", file);
   } else {
     printf("'%s' does not have an .html extension.\n", file);
