ecf04ad64390fc4d422a9ba573ecd66580a3dee7
Dalton Overmyer <overmyerms13dalton@gmail.com>
2023-03-05 01:10:45 -0600

    refactored code to make it pretty.

--- examples/split.c
+++ examples/split.c
@@ -25,16 +25,15 @@ void split_by_char(const char* input, char c) {
 
   while (!sv_is_empty(sv)) {
     sv_index_t index = sv_find_char(sv, c, 0);
-    if (index == SV_NPOS) {
-      printf("'" sv_fmt "' ", sv_arg(sv));
-      break;
-    }else if (index == 0) {
+    if (index == 0) {
       sv = sv_remove_prefix(sv, index + 1);
       continue;
     }
 
-    string_view element = sv_substr(sv, 0, index);
-    printf("'" sv_fmt "' ", sv_arg(element));
+    string_view el = sv_substr(sv, 0, index);
+    printf("'" sv_fmt "' ", sv_arg(el));
+
+    if (index == SV_NPOS) break;
 
     sv = sv_remove_prefix(sv, index + 1);
   }
@@ -43,20 +42,19 @@ void split_by_char(const char* input, char c) {
 
 void split_by_any_of(const char* input, const char* one_of) {
   string_view input_sv = sv_create_from_cstr(input);
-  string_view one_sv = sv_create_from_cstr(one_of);
+  string_view one_sv   = sv_create_from_cstr(one_of);
 
   while (!sv_is_empty(input_sv)) {
     sv_index_t index = sv_find_first_of(input_sv, one_sv, 0);
-    if (index == SV_NPOS) {
-      printf("'" sv_fmt "' ", sv_arg(input_sv));
-      break;
-    }else if (index == 0) {
+    if (index == 0) {
       input_sv = sv_remove_prefix(input_sv, index + 1);
       continue;
     }
 
-    string_view element = sv_substr(input_sv, 0, index);
-    printf("'" sv_fmt "' ", sv_arg(element));
+    string_view el = sv_substr(input_sv, 0, index);
+    printf("'" sv_fmt "' ", sv_arg(el));
+
+    if (index == SV_NPOS) break;
 
     input_sv = sv_remove_prefix(input_sv, index + 1);
   }
