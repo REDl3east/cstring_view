ee4486bdeb8689d0eb3344fe3eb26412d5f2dcf6
Dalton Overmyer <overmyerms13dalton@gmail.com>
2023-03-05 01:10:57 -0600

    added trim_whitespace example

---
+++ examples/trim_whitespace.c
@@ -0,0 +1,32 @@
+#include "sv.h"
+
+#include <stdio.h>
+
+string_view sv_trim_left_whitesapce(string_view sv);
+string_view sv_trim_right_whitesapce(string_view sv);
+string_view sv_trim_whitespace(string_view sv);
+
+int main() {
+  string_view sv;
+
+  sv = sv_trim_whitespace(sv(""));
+  printf("'" sv_fmt "'\n", sv_arg(sv));
+
+  sv = sv_trim_whitespace(sv("   \t\n   Dalton Overmyer  \r\v  "));
+  printf("'" sv_fmt "'\n", sv_arg(sv));
+
+  sv = sv_trim_whitespace(sv("       There goes my hero\n\n\n\n\n\n"));
+  printf("'" sv_fmt "'\n", sv_arg(sv));
+}
+
+string_view sv_trim_left_whitesapce(string_view sv) {
+  return sv_consume_until_first_not_of(sv, sv(" \t\n\v\f\r"));
+}
+
+string_view sv_trim_right_whitesapce(string_view sv) {
+  return sv_consume_until_last_not_of(sv, sv(" \t\n\v\f\r"));
+}
+
+string_view sv_trim_whitespace(string_view sv) {
+  return sv_trim_left_whitesapce(sv_trim_right_whitesapce(sv));
+}
