3ef848a0c51b602f583439c8beacb01e8d9a7b6c
Dalton Overmyer <overmyerms13dalton@gmail.com>
2023-08-10 20:25:52 -0500

    added glob check

--- examples/glob.c
+++ examples/glob.c
@@ -4,14 +4,22 @@
 #include <stdio.h>
 
 int sv_glob(string_view sv, string_view pattern){
-    
+
+    // find first *, consume repeating
+
     return 0;
 }
 
 int main() {
-  string_view sv = sv("hello world\n");
+  string_view sv = sv("hello world");
+  string_view pattern = sv("hello?world");
 
-  printf(sv_fmt, sv_arg(sv));
+  if(sv_glob(sv, pattern)){
+  string_view sv = sv("hello world");
+    printf("'" sv_fmt "' matches pattern '" sv_fmt "'\n", sv_arg(sv), sv_arg(pattern));
+  }else{
+    printf("'" sv_fmt "' does not match pattern '" sv_fmt "'\n", sv_arg(sv), sv_arg(pattern));
+  }
 
   return 0;
 }
