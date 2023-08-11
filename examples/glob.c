6f60e6c2c19b488ccb3047a5f10a22d1bf85b8ab
Dalton Overmyer <overmyerms13dalton@gmail.com>
2023-08-10 20:26:08 -0500

    formatting

--- examples/glob.c
+++ examples/glob.c
@@ -3,21 +3,20 @@
 
 #include <stdio.h>
 
-int sv_glob(string_view sv, string_view pattern){
+int sv_glob(string_view sv, string_view pattern) {
+  // find first *, consume repeating
 
-    // find first *, consume repeating
-
-    return 0;
+  return 0;
 }
 
 int main() {
-  string_view sv = sv("hello world");
+  string_view sv      = sv("hello world");
   string_view pattern = sv("hello?world");
 
-  if(sv_glob(sv, pattern)){
-  string_view sv = sv("hello world");
+  if (sv_glob(sv, pattern)) {
+    string_view sv = sv("hello world");
     printf("'" sv_fmt "' matches pattern '" sv_fmt "'\n", sv_arg(sv), sv_arg(pattern));
-  }else{
+  } else {
     printf("'" sv_fmt "' does not match pattern '" sv_fmt "'\n", sv_arg(sv), sv_arg(pattern));
   }
 
